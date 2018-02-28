36a98676a50a0519219831ae8683aa6b2e227be6
ryne.minor <ryne.minor@gmail.com>
2018-02-28 15:53:45 -0500

    update

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,11 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
+
+
   def add_product(product)
     current_item = line_items.find_by(product_id: product.id)
     if current_item
@@ -10,5 +15,6 @@ class Cart < ApplicationRecord
     end
     current_item
   end
+
 end
 
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,10 @@
-class LineItem < ApplicationRecord
-  belongs_to :product
-  belongs_to :cart
-end
-def total_price
-product.price * quantity
+class LineItem < ApplicationRecord 
+  belongs_to :order, optional: true
+  belongs_to :product, optional: true
+  
+  belongs_to :cart 
+
+  def total_price 
+  product.price * quantity 
+  end 
 end
